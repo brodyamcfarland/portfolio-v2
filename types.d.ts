6e6acd316b8bdd455a54600b470c0affb3d02857
brodyamcfarland <brodyamcfarland@gmail.com>
2022-09-28 19:25:42 -0700

    Added API Endpoints, Helper Functions, and Types

---
+++ types.d.ts
@@ -0,0 +1,68 @@
+interface SanityBody {
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _updatedAt: string;
+}
+
+interface Image {
+    _type: "image";
+    asset: {
+        _ref: string;
+        _type: "reference";
+    };
+}
+
+export interface PageInfo extends SanityBody {
+    _type: "pageInfo";
+    address: string;
+    backgroundInformation: string;
+    email: string;
+    role: string;
+    heroImage: Image;
+    name: string;
+    phoneNumber: string;
+    profilePic: Image;
+}
+
+export interface Skill extends SanityBody {
+    _type: "skill";
+    title: string;
+    hoverName: string;
+    image: Image;
+}
+
+export interface Technology extends SanityBody {
+    _type: "skill";
+    title: string;
+    hoverName: string;
+    image: Image;
+}
+
+export interface Project extends SanityBody {
+    _type: "project";
+    title: string;
+    image: Image;
+    summary: string;
+    technologies: Technology[];
+    demoLink: string;
+}
+
+export interface Social extends SanityBody {
+    _type: "social";
+    title: string;
+    url: string;
+}
+
+export interface Experience extends SanityBody {
+    _type: "experience";
+    jobTitle: string;
+    companyImage: Image;
+    company: string;
+    dateStarted: Date;
+    datedEnded: Date;
+    isCurrentlyWorkingHere: boolean;
+    technologies: Technology[];
+    address: string;
+    points: string[];
+}
